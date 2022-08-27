071cac273667de254aa1f157e8c32722a0899895
Gokulkannan12 <gokulkannan019@gmail.com>
2022-08-27 12:21:59 +0530

    message

---
+++ src/test/java/GitHub.java
@@ -0,0 +1,14 @@
+
+public class GitHub {
+
+   private void employeeId() {
+	System.out.println("123");
+   }
+   private void employeeName() {
+	System.out.println("Bala");
+   }
+   private void employeeDetails() {
+	System.out.println("position");
+
+}
+}
